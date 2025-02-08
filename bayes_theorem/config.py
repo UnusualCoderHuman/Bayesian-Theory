48c2feb9ab8688e24d43dce7a33819dcb5d6a855
Aryav_Lahiri <aryavlahri@gmail.com>
2025-02-08 16:03:24 +0000

    Add placeholder for Box C

--- bayes_theorem/config.py
+++ bayes_theorem/config.py
@@ -2,3 +2,8 @@ correct_answers_for_box = {
     'A': 0.5,   # Correct answer for Box A
     'B': 0.75,  # Correct answer for Box B
 }
+# Fill in number of red and blue balls?
+# Ask probablity of picking red ball?
+# Randomly select one
+# Ask to stick back in or leave out
+# Then ask what's the probablity again?
